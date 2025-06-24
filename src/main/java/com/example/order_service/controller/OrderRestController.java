b8251ea0a347787899bf4b55a4b74e83b7a72422
srinu1657 <srinu1657@gmail.com>
2025-06-23 21:31:44 -0500

    order servcie

---
+++ src/main/java/com/example/order_service/controller/OrderRestController.java
@@ -0,0 +1,20 @@
+package com.example.order_service.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderRestController {
+    @Value("${common.message}")
+    private String commonMessage;
+
+    @Value("${order.message}")
+    private String orderMessage;
+
+    @GetMapping("/order/info")
+    public String getUserInfo() {
+        return "Common: " + commonMessage + " | Order: " + orderMessage;
+    }
+
+}
